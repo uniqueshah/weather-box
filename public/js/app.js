9f63fa1426995405fadfb0cf2c774afa03e0f4ea
Shahnazar <uniqueshah>
2020-12-04 16:33:28 +0530

    remove unnecessary comment

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,7 @@
-//const { response } = require("express");
+
 //secret_key for forecast.js which is used in url is 72463e4e91fcee711de15e124f777699
 
-// console.log("client side js file is loaded");
+
 
 const weatherForm = document.querySelector('form');
 const search = document.querySelector('input');
